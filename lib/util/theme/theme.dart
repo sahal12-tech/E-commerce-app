3f159a8d523af82d678320de9f3af610987ec52a
Sahal Ansari <ansarisahal12@gmail.com>
2026-07-24 19:44:04 +0500

    theme file updated

---
+++ lib/util/theme/theme.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_catelog_page/util/theme/custom_themes/text_theme.dart';
+
+class EApptheme{
+  EApptheme._();
+
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: ETextTheme.lightTextTheme,
+  );
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.black,
+    textTheme: ETextTheme.darkTextTheme,
+  );
+}
