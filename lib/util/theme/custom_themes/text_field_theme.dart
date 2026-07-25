d37966e0ca50a6da58e552fae2b35212965c0a79
Sahal Ansari <ansarisahal12@gmail.com>
2026-07-26 00:56:05 +0500

    text_field_theme.dart

---
+++ lib/util/theme/custom_themes/text_field_theme.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class ETextFormFeildTheme {
+  ETextFormFeildTheme._();
+
+  /// Light theme
+  static InputDecorationTheme lightInputDecorationTheme = InputDecorationTheme(
+    errorMaxLines: 3,
+    prefixIconColor: Colors.grey,
+    suffixIconColor: Colors.grey,
+
+    //constraints : const BoxConstraints.expand(height: 14, inputFieldHeight),
+    labelStyle: const TextStyle().copyWith(fontSize: 14, color: Colors.black),
+    hintStyle: const TextStyle().copyWith(fontSize: 14, color: Colors.black),
+    errorStyle: const TextStyle().copyWith(fontStyle: FontStyle.normal),
+    floatingLabelStyle: const TextStyle().copyWith(color: Colors.black.withValues(alpha: 0.8)),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(color: Colors.grey),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(color: Colors.grey),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(color: Colors.black12),
+    ),
+    errorBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(color: Colors.red),
+    ),
+    focusedErrorBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(color: Colors.orange),
+    ),
+  );
+
+
+}
