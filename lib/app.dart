812c888f154d40ade95b41b05aae61a273030158
Sahal Ansari <ansarisahal12@gmail.com>
2026-07-28 14:14:44 +0500

    Creating app.dart class

---
+++ lib/app.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_catelog_page/util/theme/theme.dart';
+
+  class App extends StatelessWidget {
+    const App({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        //this line is used to system based theme, system detect dark and light mode
+        themeMode: ThemeMode.system,
+        theme: EApptheme.lightTheme,
+        darkTheme: EApptheme.darkTheme,
+      );
+    }
+  }
