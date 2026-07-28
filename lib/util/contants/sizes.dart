d7e17fd3a83c07371d554e2c6115ff7638d4f954
Sahal Ansari <ansarisahal12@gmail.com>
2026-07-28 23:59:21 +0500

    sizes.dart file created

---
+++ lib/util/contants/sizes.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+class ESizes {
+  // padding and margin sizes
+
+  static const double xs = 4.0;
+  static const double sm = 8.0;
+  static const double md = 16.0;
+  static const double lg = 24.0;
+  static const double xl = 32.0;
+
+  //icon size
+
+  static const double iconXs = 12.0;
+  static const double iconSm = 16.0;
+  static const double iconMd = 24.0;
+  static const double iconLg = 32.0;
+
+  //Font Size
+  static const double fontSizeSm = 12.0;
+  static const double fontSizeMd = 16.0;
+  static const double fontSizeLg = 24.0;
+
+  //button sizes
+  static const double buttonHeight = 18.0;
+  static const double buttonWidth = 120.0;
+  static const double buttonRadius = 12.0;
+  static const double buttonElevation = 4.0;
+
+  //app bar height
+  static const double appBarHeight = 56.0;
+
+  //Image thumb size
+  static const double imageThumbSize = 80.0;
+
+  // default spacing between sections
+
+  static const double defaultSpace = 24.0;
+  static const double spaceBtwIcons = 16.0;
+  static const double spaceBtwSections = 32.0;
+
+  //border radius
+  static const double borderRadiusSm = 4.0;
+  static const double borderRadiusMd = 8.0;
+  static const double borderRadiusLg = 10.0;
+
+  //divider height
+
+  static const double dividerHeight = 1.0;
+
+  //product item dimensions
+
+  static const double productImageSize = 120.0;
+  static const double productImageRadius = 16.0;
+  static const double productImageheight = 160.0;
+
+  //input field
+
+  static const double inputFieldRadius = 12.0;
+  static const double spaceBtwInputFields = 16.0;
+
+  //card size
+
+
+}
