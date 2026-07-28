ab665b7c64e52d6ce965298856b01a29d4fad6fd
Sahal Ansari <ansarisahal12@gmail.com>
2026-07-28 13:47:47 +0500

    Setup light and dark theme parameters in theme package

--- lib/util/theme/theme.dart
+++ lib/util/theme/theme.dart
@@ -1,5 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catelog_page/util/theme/custom_themes/text_theme.dart';
+import 'package:flutter_catelog_page/util/theme/custom_themes/appbar_theme.dart';
+import 'package:flutter_catelog_page/util/theme/custom_themes/bottom_sheet_theme.dart';
+import 'package:flutter_catelog_page/util/theme/custom_themes/checkbox_theme.dart';
+import 'package:flutter_catelog_page/util/theme/custom_themes/chip_theme.dart';
+import 'package:flutter_catelog_page/util/theme/custom_themes/elevated_button_theme.dart';
+import 'package:flutter_catelog_page/util/theme/custom_themes/outlined_button_theme.dart';
+import 'package:flutter_catelog_page/util/theme/custom_themes/text_field_theme.dart';
 
 class EApptheme{
   EApptheme._();
@@ -11,6 +18,15 @@ class EApptheme{
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.white,
     textTheme: ETextTheme.lightTextTheme,
+    chipTheme: EChipTheme.lightChipTheme,
+    appBarTheme: EAppBarTheme.lightAppBarTheme,
+    bottomSheetTheme: EBottomSheetTheme.lightBottomSheetTheme,
+    checkboxTheme: ECheckboxTheme.lightCheckboxTheme,
+    elevatedButtonTheme: EElevatedButtonTheme.lightTheme,
+    outlinedButtonTheme: EOutlinedButtonTheme.lightOutlinedButtonTheme,
+    inputDecorationTheme: ETextFormFeildTheme.lightInputDecorationTheme,
+
+
   );
   static ThemeData darkTheme = ThemeData(
     useMaterial3: true,
@@ -19,5 +35,12 @@ class EApptheme{
     primaryColor: Colors.blue,
     scaffoldBackgroundColor: Colors.black,
     textTheme: ETextTheme.darkTextTheme,
+    chipTheme: EChipTheme.darkChipTheme,
+    appBarTheme: EAppBarTheme.darkAppBarTheme,
+    bottomSheetTheme: EBottomSheetTheme.darkBottomSheetTheme,
+    checkboxTheme: ECheckboxTheme.darkCheckboxTheme,
+    elevatedButtonTheme: EElevatedButtonTheme.darkTheme,
+    outlinedButtonTheme: EOutlinedButtonTheme.darkOutlinedButtonTheme,
+    inputDecorationTheme: ETextFormFeildTheme.darkInputDecorationTheme,
   );
 }
