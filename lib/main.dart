17f909d924eeed6737042559750d54092a1d50dd
Sahal Ansari <ansarisahal12@gmail.com>
2026-07-28 14:16:15 +0500

    Setup and import app.dart class package

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_catelog_page/util/theme/theme.dart';
+//import 'package:flutter_catelog_page/util/theme/theme.dart';
+import 'app.dart';
 
 void main() {
 
@@ -11,16 +12,3 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
-  const App({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      //this line is used to system based theme, system detect dark and light mode
-      themeMode: ThemeMode.system,
-      theme: EApptheme.lightTheme,
-      darkTheme: EApptheme.darkTheme,
-    );
-  }
-}
