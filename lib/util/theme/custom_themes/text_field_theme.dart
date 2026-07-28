dbf5ea9115e160d3df0338a4edb9764a24eb33c7
Sahal Ansari <ansarisahal12@gmail.com>
2026-07-28 12:49:10 +0500

    complete the remaining dark theme mode

--- lib/util/theme/custom_themes/text_field_theme.dart
+++ lib/util/theme/custom_themes/text_field_theme.dart
@@ -36,5 +36,38 @@ class ETextFormFeildTheme {
     ),
   );
 
+  /// Dark theme
+  static InputDecorationTheme darkInputDecorationTheme = InputDecorationTheme(
+    errorMaxLines: 3,
+    prefixIconColor: Colors.grey,
+    suffixIconColor: Colors.grey,
+
+    //constraints : const BoxConstraints.expand(height: 14, inputFieldHeight),
+    labelStyle: const TextStyle().copyWith(fontSize: 14, color: Colors.white),
+    hintStyle: const TextStyle().copyWith(fontSize: 14, color: Colors.white),
+    errorStyle: const TextStyle().copyWith(fontStyle: FontStyle.normal),
+    floatingLabelStyle: const TextStyle().copyWith(color: Colors.black.withValues(alpha: 0.8)),
+    border: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(color: Colors.grey),
+    ),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(color: Colors.grey),
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(color: Colors.white),
+    ),
+    errorBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(color: Colors.red),
+    ),
+    focusedErrorBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(14),
+      borderSide: const BorderSide(color: Colors.orange),
+    ),
+  );
+
 
 }
