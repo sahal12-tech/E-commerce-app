34a02ea1369898bb3d66ff2464eda84086b81995
Sahal Ansari <ansarisahal12@gmail.com>
2026-07-30 14:36:54 +0500

    Complete image_string.dart

--- lib/util/contants/image_strings.dart
+++ lib/util/contants/image_strings.dart
@@ -3,6 +3,11 @@ import 'package:flutter/material.dart';
 class EImages{
   //app logos
 
-  static const darkAppLogo = "assets/logos/E-store-splash-screen-dark.png";
-  static const lightAppLogo = "assets/logos/E-store-splash-screen-light.png";
+  static const String darkAppLogo = "assets/logos/E-store-splash-screen-dark.png";
+  static const  String lightAppLogo = "assets/logos/E-store-splash-screen-light.png";
+
+  //social logos
+  static const String google = "assets/logos/google-icon.png";
+  static const String facebook = "assets/logos/facebook-icon.png";
+
 }
