6218fda6b4b87a9222d289cef492a4a67c7f168b
Sahal Ansari <ansarisahal12@gmail.com>
2026-07-30 15:17:35 +0500

    fixed comment section

---
+++ lib/util/contants/enums.dart
@@ -0,0 +1,8 @@
+//List of enums
+//they cannot be creates in class.
+
+enum TextSizes {small, medium, large}
+
+enum OrderStatus { pending, shipped, delivered }
+
+enum PaymentMethod { creditCard, masterCard, visa ,paypal, bankTransfer, easypaisa, jazzCash }
