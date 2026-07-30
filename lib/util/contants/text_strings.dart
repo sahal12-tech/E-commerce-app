23ba860a1969bf20aca419539d7823f01a3ed028
Sahal Ansari <ansarisahal12@gmail.com>
2026-07-30 14:58:29 +0500

    Text String .dart

---
+++ lib/util/contants/text_strings.dart
@@ -0,0 +1,21 @@
+// This class String contain all text
+class ETexts{
+  //--Global Texts
+
+  //--onboarding text
+
+  static const String onBoardTitle1 = "Choose Your Product";
+  static const String onBoardTitle2 = "Select Payment Method";
+  static const String onBoardTitle3 = "Deliver at your door step";
+
+  static const String onBoardSubTitle1 = "Discover thousands of products curated just for you – from trending styles to everyday essentials.";
+  static const String onBoardSubTitle2 = "Get smart recommendations, exclusive deals, and flash sales tailored to your taste.";
+  static const String onBoardSubTitle3 = "Shop with confidence – secure payments, real-time order tracking, and hassle-free delivery.";
+
+  //home
+
+  static const String homeAppBarTitle = "Shop. Smile. Repeat.";
+  static const String homeAppBarSubTitle = "Sahal Ansari";
+
+
+}
