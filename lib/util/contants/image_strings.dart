a9eb9d07e839baf571993fcc843b2d74cb2863d4
Sahal Ansari <ansarisahal12@gmail.com>
2026-07-30 14:20:22 +0500

    Images_String.dart

---
+++ lib/util/contants/image_strings.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class EImages{
+  //app logos
+
+  static const darkAppLogo = "assets/logos/E-store-splash-screen-dark.png";
+  static const lightAppLogo = "assets/logos/E-store-splash-screen-light.png";
+}
