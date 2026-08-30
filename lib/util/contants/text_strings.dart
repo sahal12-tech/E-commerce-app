ae238f1e28561f5d4bf182626f48d1f2d085447e
Sahal Ansari <ansarisahal12@gmail.com>
2026-08-30 13:23:10 +0500

    Completing the Etext String

--- lib/util/contants/text_strings.dart
+++ lib/util/contants/text_strings.dart
@@ -1,26 +1,50 @@
-// This class String contain all text
-class ETexts{
-  //--Global Texts
+/// This class contains all the App Text in String format.
+class ETexts {
+  // Global Texts
+  static const String submit = "Submit";
+  static const String appName = "T-Store";
+  static const String tContinue = "Continue";
 
-  //--onboarding text
+  // -- OnBoarding Texts
+  static const String onBoardingTitle1 = "Choose your product";
+  static const String onBoardingTitle2 = "Select Payment Method";
+  static const String onBoardingTitle3 = "Deliver at your door step";
 
-  static const String onBoardTitle1 = "Choose Your Product";
-  static const String onBoardTitle2 = "Select Payment Method";
-  static const String onBoardTitle3 = "Deliver at your door step";
+  static const String onBoardingSubTitle1 = "Welcome to a World of Limitless Choices - Your Perfect Item Awaits!";
+  static const String onBoardingSubTitle2 = "For Seamless Transactions, Choose Your Payment Path - Your Convenience, Our Priority!";
+  static const String onBoardingSubTitle3 = "From Our Doorstep to Yours - Swift, Secure, and Contactless Delivery!";
 
-  static const String onBoardSubTitle1 = "Discover thousands of products curated just for you – from trending styles to everyday essentials.";
-  static const String onBoardSubTitle2 = "Get smart recommendations, exclusive deals, and flash sales tailored to your taste.";
-  static const String onBoardSubTitle3 = "Shop with confidence – secure payments, real-time order tracking, and hassle-free delivery.";
+  // -- Authentication Heading Texts
+  static const String loginTitle = "Welcome back,";
+  static const String loginSubTitle = "Discover Limitless Choices and Unmatched Convenience.";
+  static const String signupTitle = "Let's create your account";
+  static const String forgetPasswordTitle = "Forget password";
+  static const String forgetPasswordSubTitle = "Don't worry sometimes people can forget too, enter your email and we will send you a password reset link.";
+  static const String changePasswordTitle = "Password Reset Email Sent";
+  static const String changePasswordSubTitle = "Your Account Security is Our Priority! We've Sent You a Secure Link to Safely Change Your Password and Keep Your Account Protected.";
+  static const String confirmEmail = "Verify your email address!";
+  static const String confirmEmailSubTitle = "Congratulations! Your Account Awaits: Verify Your Email to Start Shopping and Experience a World of Unrivaled Deals and Personalized Offers.";
+  static const String yourAccountCreatedTitle = "Your account successfully created!";
+  static const String yourAccountCreatedSubTitle = "Welcome to Your Ultimate Shopping Destination: Your Account is Created, Unleash the Joy of Seamless Online Shopping!";
 
-  //home
-
-  static const String homeAppBarTitle = "Shop. Smile. Repeat.";
-  static const String homeAppBarSubTitle = "Sahal Ansari";
-
-  static const String LoginTitle = "Welcome Back";
-  static const String LoginSubTitle = "Shop. Smile. Repeat.";
-
-  //authentication Text
-
-  static const String email = "Email";
+  // -- Authentication Form Texts
+  static const String firstName = "First Name";
+  static const String lastName = "Last Name";
+  static const String username = "Username";
+  static const String email = "E-Mail";
+  static const String password = "Password";
+  static const String newPassword = "New Password";
+  static const String phoneNo = "Phone Number";
+  static const String rememberMe = "Remember Me";
+  static const String forgetPassword = "Forget Password?";
+  static const String signIn = "Sign In";
+  static const String createAccount = "Create Account";
+  static const String orSignInWith = "or sign in with";
+  static const String orSignUpWith = "or sign up with";
+  static const String iAgreeTo = "I agree to";
+  static const String privacyPolicy = "Privacy Policy";
+  static const String termsOfUse = "Terms of use";
+  static const String verificationCode = "verificationCode";
+  static const String resendEmail = "Resend Email";
+  static const String resendEmailIn = "Resend email in";
 }
