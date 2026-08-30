4cec05c21350ba90d2def69cd2bb12c9fdc16716
Sahal Ansari <ansarisahal12@gmail.com>
2026-08-30 13:02:44 +0500

    Create common spacing

---
+++ lib/common/styles/ESpacingStyle.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+import '../../../../util/contants/sizes.dart';
+
+class ESpacingStyle {
+  static const EdgeInsetsGeometry paddingWithAppBarHeight = EdgeInsets.only(
+    top: ESizes.appBarHeight,
+    left: ESizes.defaultSpace,
+    right: ESizes.defaultSpace,
+    bottom: ESizes.defaultSpace,
+  );
+}
