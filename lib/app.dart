b20e492875d31f8dfeb6eaed7904f331f7006122
Sahal Ansari <ansarisahal12@gmail.com>
2026-08-30 14:28:10 +0500

    Setup login screen to app.dart

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catelog_page/features/authentication/screens/login/login.dart';
 import 'package:flutter_catelog_page/util/theme/theme.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 
-  class App extends StatelessWidget {
-    const App({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-        //this line is used to system based theme, system detect dark and light mode
-        themeMode: ThemeMode.system,
-        theme: EApptheme.lightTheme,
-        darkTheme: EApptheme.darkTheme,
-      );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.system,
+      theme: EApptheme.lightTheme,
+      darkTheme: EApptheme.darkTheme,
+      home: const LoginScreen(),
+    );
   }
+}
