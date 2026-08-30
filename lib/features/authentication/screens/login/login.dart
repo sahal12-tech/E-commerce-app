6746e7203d9f46f13e373b6bb78dfa3eef798045
Sahal Ansari <ansarisahal12@gmail.com>
2026-08-30 14:17:06 +0500

    Completed the form and button section

--- lib/features/authentication/screens/login/login.dart
+++ lib/features/authentication/screens/login/login.dart
@@ -36,29 +36,60 @@ class LoginScreen extends StatelessWidget {
               ),
 
               /// Login Form
-              Form(child: Column(
-                children: [
-                  ///Email
-                  TextFormField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Iconsax.direct_right),
-                      labelText: ETexts.email
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: ESizes.spaceBtwSections),
+                child: Form(child: Column(
+                  children: [
+                    ///Email
+                    TextFormField(
+                      decoration: InputDecoration(
+                        prefixIcon: Icon(Iconsax.direct_right),
+                        labelText: ETexts.email
+                      ),
                     ),
-                  ),
 
-                  const SizedBox(height: ESizes.spaceBtwInputFields,),
+                    const SizedBox(height: ESizes.spaceBtwInputFields,),
 
-                  TextFormField(
-                    decoration: InputDecoration(
-                        prefixIcon: Icon(Iconsax.password_check),
-                        labelText: ETexts.password,
-                        suffixIcon: Icon(Iconsax.eye)
+                    TextFormField(
+                      decoration: InputDecoration(
+                          prefixIcon: Icon(Iconsax.password_check),
+                          labelText: ETexts.password,
+                          suffixIcon: Icon(Iconsax.eye_slash)
+                      ),
                     ),
-                  )
-                ],
-              ),
+                    const SizedBox(height: ESizes.spaceBtwInputFields/2,),
+
+                    ///remember me & Forgot Button
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            ///Remember Me
+                            Checkbox(value: true, onChanged:(value){}),
+                            const Text(ETexts.rememberMe),
+                          ],
+                        ),
+                        ///Forgot Password
+                        TextButton(onPressed: (){}, child: Text(ETexts.forgetPassword))
+                      ],
+                    ),
+                    const SizedBox(height: ESizes.spaceBtwSections,),
+                    ///SignIn button
+                    SizedBox( width: double.infinity,
+                        child:
+                        ElevatedButton(onPressed: (){}, child: Text(ETexts.signIn))
+                    ),
+
+                    ///Create Account Button
+                    SizedBox( width: double.infinity,
+                        child:
+                        OutlinedButton(onPressed: (){}, child: Text(ETexts.createAccount))
+                    ),
+                  ],
+                ),
+                ),
               )
-
             ],
           ),
         )
