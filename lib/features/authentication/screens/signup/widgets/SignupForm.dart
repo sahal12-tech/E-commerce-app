eee686adbef1070a6bd57a70d7c42d9d9ceb94e4
Sahal Ansari <ansarisahal12@gmail.com>
2026-09-02 18:47:28 +0500

    Sign_up widgets

---
+++ lib/features/authentication/screens/signup/widgets/SignupForm.dart
@@ -0,0 +1,128 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import '../../../../../util/constants/colors.dart';
+import '../../../../../util/constants/sizes.dart';
+import '../../../../../util/constants/text_strings.dart';
+import '../../../../../util/helpers/helper_functions.dart';
+
+class SignupForm extends StatelessWidget {
+  const SignupForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final dark = EHelperFunctions.isdarkMode(context);
+    return Form(
+      child: Column(
+        children: [
+          Row(
+            children: [
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: ETexts.firstName,
+                    prefixIcon: Icon(Iconsax.user),
+                  ),
+                ),
+              ),
+              const SizedBox(width: ESizes.spaceBtwInputFields),
+              Expanded(
+                child: TextFormField(
+                  decoration: InputDecoration(
+                    labelText: ETexts.lastName,
+                    prefixIcon: Icon(Iconsax.user),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          const SizedBox(height: ESizes.spaceBtwInputFields),
+
+          /// user name
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: ETexts.username,
+              prefixIcon: Icon(Iconsax.user_edit),
+            ),
+          ),
+
+          const SizedBox(height: ESizes.spaceBtwInputFields),
+
+          /// Email
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: ETexts.email,
+              prefixIcon: Icon(Iconsax.direct_right),
+            ),
+          ),
+
+          const SizedBox(height: ESizes.spaceBtwInputFields),
+
+          /// Phone number
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: ETexts.phoneNo,
+              prefixIcon: Icon(Iconsax.call),
+            ),
+          ),
+
+          const SizedBox(height: ESizes.spaceBtwInputFields),
+
+          ///password
+          TextFormField(
+            decoration: InputDecoration(
+              prefixIcon: Icon(Iconsax.password_check),
+              labelText: ETexts.password,
+              suffixIcon: Icon(Iconsax.eye_slash),
+            ),
+          ),
+
+          const SizedBox(height: ESizes.spaceBtwSections),
+
+          ///Terms and Conditions
+          Row(
+            children: [
+              SizedBox(
+                width: 24,
+                height: 24,
+                child: Checkbox(value: true, onChanged: (value) {}),
+              ),
+              const SizedBox(height: ESizes.spaceBtwitems),
+
+              Text.rich(
+                TextSpan(
+                  children: [
+                    TextSpan(
+                      text: '${ETexts.iAgreeTo} ',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                    TextSpan(
+                      text: '${ETexts.privacyPolicy} ',
+                      style: Theme.of(context).textTheme.bodyMedium!.apply(
+                        color: dark ? EColors.white : EColors.primary,
+                        decoration: TextDecoration.underline,
+                        decorationColor: dark ? EColors.white : EColors.primary,
+                      ),
+                    ),
+                    TextSpan(
+                      text: '${ETexts.and} ',
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+
+                    TextSpan(
+                      text: '${ETexts.termsOfUse} ',
+                      style: Theme.of(context).textTheme.bodyMedium!.apply(
+                        color: dark ? EColors.white : EColors.primary,
+                        decoration: TextDecoration.underline,
+                        decorationColor: dark ? EColors.white : EColors.primary,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/features/authentication/screens/signup/widgets/signup.dart
+++ lib/features/authentication/screens/signup/widgets/signup.dart
@@ -2,18 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_catelog_page/common/widgets/login_signup/form_divider.dart';
 import 'package:flutter_catelog_page/util/constants/sizes.dart';
 import 'package:flutter_catelog_page/util/constants/text_strings.dart';
-import 'package:iconsax/iconsax.dart';
-
 import '../../../../../common/widgets/login_signup/ESocialLogin.dart';
-import '../../../../../util/constants/colors.dart';
-import '../../../../../util/helpers/helper_functions.dart';
+import 'SignupForm.dart';
 
 class SignupScreen extends StatelessWidget {
   const SignupScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final dark = EHelperFunctions.isdarkMode(context);
     return Scaffold(
       appBar: AppBar(),
       body: SingleChildScrollView(
@@ -31,136 +27,14 @@ class SignupScreen extends StatelessWidget {
               const SizedBox(height: ESizes.spaceBtwSections),
 
               /// Form
-              Form(
-                child: Column(
-                  children: [
-                    Row(
-                      children: [
-                        Expanded(
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                              labelText: ETexts.firstName,
-                              prefixIcon: Icon(Iconsax.user),
-                            ),
-                          ),
-                        ),
-                        const SizedBox(width: ESizes.spaceBtwInputFields),
-                        Expanded(
-                          child: TextFormField(
-                            decoration: InputDecoration(
-                              labelText: ETexts.lastName,
-                              prefixIcon: Icon(Iconsax.user),
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                    const SizedBox(height: ESizes.spaceBtwInputFields),
-
-                    /// user name
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: ETexts.username,
-                        prefixIcon: Icon(Iconsax.user_edit),
-                      ),
-                    ),
-
-                    const SizedBox(height: ESizes.spaceBtwInputFields),
-
-                    /// Email
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: ETexts.email,
-                        prefixIcon: Icon(Iconsax.direct_right),
-                      ),
-                    ),
-
-                    const SizedBox(height: ESizes.spaceBtwInputFields),
-
-                    /// Phone number
-                    TextFormField(
-                      decoration: InputDecoration(
-                        labelText: ETexts.phoneNo,
-                        prefixIcon: Icon(Iconsax.call),
-                      ),
-                    ),
-
-                    const SizedBox(height: ESizes.spaceBtwInputFields),
-
-                    ///password
-                    TextFormField(
-                      decoration: InputDecoration(
-                        prefixIcon: Icon(Iconsax.password_check),
-                        labelText: ETexts.password,
-                        suffixIcon: Icon(Iconsax.eye_slash),
-                      ),
-                    ),
-
-                    const SizedBox(height: ESizes.spaceBtwSections),
-
-                    ///Terms and Conditions
-                    Row(
-                      children: [
-                        SizedBox(
-                          width: 24,
-                          height: 24,
-                          child: Checkbox(value: true, onChanged: (value) {}),
-                        ),
-                        const SizedBox(height: ESizes.spaceBtwitems),
-
-                        Text.rich(
-                          TextSpan(
-                            children: [
-                              TextSpan(
-                                text: '${ETexts.iAgreeTo} ',
-                                style: Theme.of(context).textTheme.bodySmall,
-                              ),
-                              TextSpan(
-                                text: '${ETexts.privacyPolicy} ',
-                                style: Theme.of(context).textTheme.bodyMedium!
-                                    .apply(
-                                      color: dark
-                                          ? EColors.white
-                                          : EColors.primary,
-                                      decoration: TextDecoration.underline,
-                                      decorationColor: dark
-                                          ? EColors.white
-                                          : EColors.primary,
-                                    ),
-                              ),
-                              TextSpan(
-                                text: '${ETexts.and} ',
-                                style: Theme.of(context).textTheme.bodySmall,
-                              ),
-
-                              TextSpan(
-                                text: '${ETexts.termsOfUse} ',
-                                style: Theme.of(context).textTheme.bodyMedium!
-                                    .apply(
-                                      color: dark
-                                          ? EColors.white
-                                          : EColors.primary,
-                                      decoration: TextDecoration.underline,
-                                      decorationColor: dark
-                                          ? EColors.white
-                                          : EColors.primary,
-                                    ),
-                              ),
-                            ],
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ),
+              const SignupForm(),
 
               const SizedBox(height: ESizes.spaceBtwSections),
 
               ///Create Account Button
               SizedBox(
                 width: double.infinity,
-                child: OutlinedButton(
+                child: ElevatedButton(
                   onPressed: () {},
                   child: Text(ETexts.createAccount),
                 ),
