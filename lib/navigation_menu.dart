b6c61c83208e3201f839408e29e1474594e2ab61
Sahal Ansari <ansarisahal12@gmail.com>
2026-09-06 00:21:38 +0500

    navigation controller done

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 
 class NavigationMenu extends StatelessWidget {
@@ -6,18 +7,48 @@ class NavigationMenu extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
     return Scaffold(
-
-      bottomNavigationBar: NavigationBar(
-
-        height: 80,
-        destinations: [
-          const NavigationDestination(icon: Icon(Iconsax.home), label: 'Home'),
-          const NavigationDestination(icon: Icon(Iconsax.shop), label: 'Shop'),
-          const NavigationDestination(icon: Icon(Iconsax.heart), label: 'Wishlist'),
-          const NavigationDestination(icon: Icon(Iconsax.user), label: 'Profile'),
-        ],
+      bottomNavigationBar: Obx(
+        () => NavigationBar(
+          height: 80,
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected: (index) =>
+              controller.selectedIndex.value = index,
+          destinations: [
+            const NavigationDestination(
+              icon: Icon(Iconsax.home),
+              label: 'Home',
+            ),
+            const NavigationDestination(
+              icon: Icon(Iconsax.shop),
+              label: 'Shop',
+            ),
+            const NavigationDestination(
+              icon: Icon(Iconsax.heart),
+              label: 'Wishlist',
+            ),
+            const NavigationDestination(
+              icon: Icon(Iconsax.user),
+              label: 'Profile',
+            ),
+          ],
+        ),
+      ),
+      body: Obx(
+        () => controller.screens[controller.selectedIndex.value],
       ),
     );
   }
+}
+
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
+
+  final screens = [
+    Container(color: Colors.blue),
+    Container(color: Colors.green),
+    Container(color: Colors.red),
+    Container(color: Colors.yellow),
+  ];
 }
