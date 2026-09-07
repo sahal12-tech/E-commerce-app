0d433e42068b8386c8e46507603550c843edb317
Sahal Ansari <ansarisahal12@gmail.com>
2026-09-07 17:27:38 +0500

    Working on home screen

--- lib/features/store/Home/home.dart
+++ lib/features/store/Home/home.dart
@@ -1,12 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catelog_page/util/constants/colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              color: EColors.primary,
+              padding: const EdgeInsets.all(0),
+              child: Stack(
+                children: [
+                  Container(
+                    width: 400,
+                    height: 400,
+                    padding: EdgeInsets.all(0),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(400),
+                        color: EColors.textWhite.withValues(alpha: 0.1)
+                    ),
+                  ),
+                  Container(
+                    width: 400,
+                    height: 400,
+                    padding: EdgeInsets.all(0),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(400),
+                        color: EColors.textWhite.withValues(alpha: 0.1)
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
     );
   }
 }
