647035b8c28123fd1578a5d2549555faef961d11
Sahal Ansari <ansarisahal12@gmail.com>
2026-09-18 21:53:54 +0500

    Working on Store Sreens's Sliver Scroll view

--- lib/features/store/Store/store.dart
+++ lib/features/store/Store/store.dart
@@ -1,25 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catelog_page/common/widgets/AppBar/appbar.dart';
+import 'package:flutter_catelog_page/common/widgets/custom_shape/containers/ESearchContainer.dart';
 import 'package:flutter_catelog_page/common/widgets/products/cart/cart_menu_icon.dart';
+import 'package:flutter_catelog_page/util/constants/colors.dart';
+import 'package:flutter_catelog_page/util/constants/sizes.dart';
+import 'package:flutter_catelog_page/util/helpers/helper_functions.dart';
 
 class StoreScreen extends StatelessWidget {
   const StoreScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final dark = EHelperFunctions.isdarkMode(context);
     return Scaffold(
       appBar: EAppBar(
         showBackArrow: false,
-        title: Text("Store", style: Theme.of(context).textTheme.headlineMedium,),
+        title: Text("Store", style: Theme.of(context).textTheme.headlineMedium),
         actions: [ECartCounterIcon(count: 4, onTap: () {})],
       ),
-    body: NestedScrollView(headerSliverBuilder:(_,innerBoxIsScorlled) {
-      return [
-        SliverAppBar(
-
-        )
-      ];
-    }, body: Container()),
+      body: NestedScrollView(
+        headerSliverBuilder: (_, innerBoxIsScorlled) {
+          return [
+            SliverAppBar(
+              pinned: true,
+              floating: true,
+              backgroundColor: dark ? EColors.black : EColors.white,
+              expandedHeight: 440,
+              automaticallyImplyLeading: false,
+              flexibleSpace: Padding(
+                padding: EdgeInsetsGeometry.all(ESizes.defaultSpace),
+                child: ListView(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  children: const [
+                    ///Search Bar
+                    SizedBox(height: ESizes.spaceBtwitems,),
+                    ESearchContainer(text: 'Search in Store',showBorder: true,),
+                    SizedBox(height: ESizes.spaceBtwSections,)
+                  ],
+                ),
+              ),
+            ),
+          ];
+        },
+        body: Container(),
+      ),
     );
   }
 }
