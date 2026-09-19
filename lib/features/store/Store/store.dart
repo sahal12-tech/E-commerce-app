d7052e81b39609d35733ee7f04f2fe563c87fe78
Sahal Ansari <ansarisahal12@gmail.com>
2026-09-19 14:59:11 +0500

    Working on grid layout

--- lib/features/store/Store/store.dart
+++ lib/features/store/Store/store.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_catelog_page/common/widgets/AppBar/appbar.dart';
 import 'package:flutter_catelog_page/common/widgets/custom_shape/containers/ERounded_Container.dart';
 import 'package:flutter_catelog_page/common/widgets/custom_shape/containers/ESearchContainer.dart';
+import 'package:flutter_catelog_page/common/widgets/layout/grid_layout.dart';
 import 'package:flutter_catelog_page/common/widgets/products/cart/cart_menu_icon.dart';
+import 'package:flutter_catelog_page/common/widgets/texts/EBrand_Title_With_Verify_Icon.dart';
 import 'package:flutter_catelog_page/common/widgets/texts/section_heading.dart';
 import 'package:flutter_catelog_page/util/constants/colors.dart';
+import 'package:flutter_catelog_page/util/constants/enums.dart';
 import 'package:flutter_catelog_page/util/constants/image_strings.dart';
 import 'package:flutter_catelog_page/util/constants/sizes.dart';
 import 'package:flutter_catelog_page/util/helpers/helper_functions.dart';
@@ -52,31 +55,53 @@ class StoreScreen extends StatelessWidget {
                       showActionButton: true,
                       onPressed: () {},
                     ),
+                    const SizedBox(height: ESizes.spaceBtwitems / 1.5),
 
-                    ERoundedContainer(
+                    EGridLayout(
+                      mainAxisExtent: 80,
+                      itemCount: 4,
+                      itemBuilder: (_, index) {
+                        return GestureDetector(
+                          onTap: () {},
+                          child: ERoundedContainer(
+                            padding: EdgeInsetsGeometry.all(ESizes.sm),
+                            showBorder: true,
+                            backgroundColor: Colors.transparent,
+                            borderColor: EHelperFunctions.isdarkMode(context)
+                                ? EColors.light
+                                : EColors.grey,
+                            child: Row(
+                              children: [
+                                ///Icon
+                                Flexible(child: const ECircularIconImage()),
 
-                      padding: EdgeInsetsGeometry.all(ESizes.sm),
-                      showBorder: true,
-                      backgroundColor: Colors.transparent,
-                      borderColor: EHelperFunctions.isdarkMode(context)
-                          ? EColors.light
-                          : EColors.grey,
-                      child: Row(
-                        children: [
-                          ///Icon
-                          const ECircularIconImage(),
+                                const SizedBox(width: ESizes.spaceBtwitems / 2),
 
-                          const SizedBox(width: ESizes.spaceBtwitems / 2,),
-
-
-                          ///--Text
-                          Column(
-                            children: [
-
-                            ],
-                          )
-                        ],
-                      ),
+                                ///--Text
+                                Expanded(
+                                  child: Column(
+                                    mainAxisSize: MainAxisSize.min,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      const EBrandTitleWithVerifiedIcon(
+                                        title: 'Nike',
+                                        brandTextSize: TextSizes.large,
+                                      ),
+                                      Text(
+                                        "256 Products of abcd of hij",
+                                        overflow: TextOverflow.ellipsis,
+                                        style: Theme.of(
+                                          context,
+                                        ).textTheme.labelMedium,
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      },
                     ),
                   ],
                 ),
@@ -91,9 +116,7 @@ class StoreScreen extends StatelessWidget {
 }
 
 class ECircularIconImage extends StatelessWidget {
-  const ECircularIconImage({
-    super.key,
-  });
+  const ECircularIconImage({super.key});
 
   @override
   Widget build(BuildContext context) {
